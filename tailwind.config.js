867b27f6dd53a80e606cf1203d9b4759d6ff3f97
Etienne Beaudry Auger <etienne@beaudry-auger.com>
2025-04-28 16:38:33 -0400

    feat: implement minimal Dioxus desktop app with hot-reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "all",
-  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
+  content: [
+    "./src/**/*.{rs,html,css}",
+  ],
   theme: {
     extend: {},
   },
